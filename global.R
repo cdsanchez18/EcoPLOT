ef3ac198c43c60eaaa8612c3be2a05f62bbc2cea
cdsanchez18 <72049448+cdsanchez18@users.noreply.github.com>
2021-05-11 13:17:08 -0700

    updated package downloads

--- global.R
+++ global.R
@@ -2,7 +2,8 @@
 #Cran
 required.packages <- c("shiny", "readxl", "treeio", "colourpicker", "ggtree", "shinyjs",
                        "ggplot2", "vegan", "tidyr", "lubridate", "dplyr", "reshape2", "stringr", "shinythemes", #"shinycssloaders",
-                       "RColorBrewer", "DT", "htmltools", "dataPreparation", "iRF", "doParallel", "BiocGenerics", "plotly", "randomForest")
+                       "RColorBrewer", "DT", "htmltools", "dataPreparation", #"iRF", 
+                       "doParallel", "BiocGenerics", "plotly", "randomForest")
 new.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
 
@@ -16,7 +17,7 @@ if(length(new.packages.biocmanager)) BiocManager::install(new.packages.biocmanag
 #Devtools
 if(!requireNamespace("devtools", quietly = TRUE))
   install.packages("devtools")
-required.packages.devtools <- c("kalimu/shinyEventLogger", "CDSanchez18/EcoPLOT","jbisanz/qiime2R")
+required.packages.devtools <- c("kalimu/shinyEventLogger", "CDSanchez18/EcoPLOT","jbisanz/qiime2R", "karlkumbier/iRF2.0")
 required.packages.devtools2 <- c("shinyEventLogger", "EcoPLOT", "qiime2R")
 new.packages.devtools <- required.packages.devtools[!(required.packages.devtools %in% installed.packages()[,"Package"])]
 if(length(new.packages.devtools)) devtools::install_github(new.packages.devtools, dependencies = TRUE)
