3c0e1e8ec6d93c83c0fb68253602502955735aed
cdsanchez18 <72049448+cdsanchez18@users.noreply.github.com>
2021-09-23 13:13:10 -0700

    Update EcoPLOTServer.R
    Updated server file to reflect the location changes of certain files.

--- EcoPLOTServer.R
+++ EcoPLOTServer.R
@@ -12,56 +12,56 @@ EcoPLOTServer <- shinyServer(function(input, output, session){
   ##Log Data
   set_logging_session()
       #Phenotype
-        source("modules/Phenotype/datasetLOG.R", local = TRUE)
-        source("modules/Phenotype/filterLOG.R", local = TRUE)
-        source("modules/Phenotype/parametricLOG.R", local = TRUE)
-        source("modules/Phenotype/nonparametricLOG.R", local = TRUE)
+        source("modules/Phenotype/Logs/datasetLOG.R", local = TRUE)
+        source("modules/Phenotype/Logs/filterLOG.R", local = TRUE)
+        source("modules/Phenotype/Logs/parametricLOG.R", local = TRUE)
+        source("modules/Phenotype/Logs/nonparametricLOG.R", local = TRUE)
       #Geochemistry
-        source("modules/Geochemistry/datasetLOG.R", local = TRUE)
-        source("modules/Geochemistry/filterLOG.R", local = TRUE)
-        source("modules/Geochemistry/parametricLOG.R", local = TRUE)
-        source("modules/Geochemistry/nonparametricLOG.R", local = TRUE)
+        source("modules/Geochemistry/Logs/datasetLOG.R", local = TRUE)
+        source("modules/Geochemistry/Logs/filterLOG.R", local = TRUE)
+        source("modules/Geochemistry/Logs/parametricLOG.R", local = TRUE)
+        source("modules/Geochemistry/Logs/nonparametricLOG.R", local = TRUE)
       #Amplicon
-        source("modules/Amplicon/datasetLOG.R", local = TRUE)
-        source("modules/Amplicon/filterLOG.R", local = TRUE)
+        source("modules/Amplicon/Logs/datasetLOG.R", local = TRUE)
+        source("modules/Amplicon/Logs/filterLOG.R", local = TRUE)
       #iRF
-        source("modules/iRF/iRFLOG.R", local = TRUE)
+        source("modules/iRF/Logs/iRFLOG.R", local = TRUE)
   
   ##Beginning of app section
   
   ##Plant Phenotypic Data Server
-  source("modules/Phenotype/upload.R", local = TRUE)
-  source("modules/Phenotype/filter.R", local = TRUE)
-  source("modules/Phenotype/plot.R", local = TRUE)
-  source("modules/Phenotype/parametric.R", local = TRUE)
-  source("modules/Phenotype/nonparametric.R", local = TRUE)
-  source("modules/Phenotype/misc.R", local = TRUE)
+  source("modules/Phenotype/Sections/upload.R", local = TRUE)
+  source("modules/Phenotype/Sections/filter.R", local = TRUE)
+  source("modules/Phenotype/Sections/plot.R", local = TRUE)
+  source("modules/Phenotype/Sections/parametric.R", local = TRUE)
+  source("modules/Phenotype/Sections/nonparametric.R", local = TRUE)
+  source("modules/Phenotype/Sections/misc.R", local = TRUE)
   
   ##Geochemistry
-  source("modules/Geochemistry/upload.R", local = TRUE)
-  source("modules/Geochemistry/filter.R", local = TRUE)
-  source("modules/Geochemistry/plot.R", local = TRUE)
-  source("modules/Geochemistry/parametric.R", local = TRUE)
-  source("modules/Geochemistry/nonparametric.R", local = TRUE)
-  source("modules/Geochemistry/soilindexes.R", local = TRUE)
-  source("modules/Geochemistry/misc.R", local = TRUE)
+  source("modules/Geochemistry/Sections/upload.R", local = TRUE)
+  source("modules/Geochemistry/Sections/filter.R", local = TRUE)
+  source("modules/Geochemistry/Sections/plot.R", local = TRUE)
+  source("modules/Geochemistry/Sections/parametric.R", local = TRUE)
+  source("modules/Geochemistry/Sections/nonparametric.R", local = TRUE)
+  source("modules/Geochemistry/Sections/soilindexes.R", local = TRUE)
+  source("modules/Geochemistry/Sections/misc.R", local = TRUE)
   
   
   ###Amplicon Data
-  source("modules/Amplicon/upload.R", local = TRUE)
-  source("modules/Amplicon/summaries.R", local = TRUE)
-  source("modules/Amplicon/phylotree.R", local = TRUE)
-  source("modules/Amplicon/barplot.R", local = TRUE)
-  source("modules/Amplicon/alphadiv.R", local = TRUE)
-  source("modules/Amplicon/betadiv.R", local = TRUE)
-  source("modules/Amplicon/heatmap.R", local = TRUE)
-  source("modules/Amplicon/filter.R", local = TRUE)
-  source("modules/Amplicon/diffabundance.R", local = TRUE)
-  source("modules/Amplicon/misc.R", local = TRUE)
+  source("modules/Amplicon/Sections/upload.R", local = TRUE)
+  source("modules/Amplicon/Sections/summaries.R", local = TRUE)
+  source("modules/Amplicon/Sections/phylotree.R", local = TRUE)
+  source("modules/Amplicon/Sections/barplot.R", local = TRUE)
+  source("modules/Amplicon/Sections/alphadiv.R", local = TRUE)
+  source("modules/Amplicon/Sections/betadiv.R", local = TRUE)
+  source("modules/Amplicon/Sections/heatmap.R", local = TRUE)
+  source("modules/Amplicon/Sections/filter.R", local = TRUE)
+  source("modules/Amplicon/Sections/diffabundance.R", local = TRUE)
+  source("modules/Amplicon/Sections/misc.R", local = TRUE)
   
   ###IRF 
-  source("modules/IRF/dataformatting.R", local = TRUE)
-  source("modules/IRF/misc.R", local = TRUE)
+  source("modules/IRF/Sections/dataformatting.R", local = TRUE)
+  source("modules/IRF/Sections/misc.R", local = TRUE)
   
 })
 
